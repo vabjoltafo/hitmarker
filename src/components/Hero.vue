c70e84c3f52a2c33f254bb45da9f224b8897da78
Vabjol Tafo <vabjoltafo@outlook.com>
2021-08-13 20:27:22 +0200

    Big update

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -33,41 +33,8 @@
         </div>
       </div>
       
-       <div
-        class="relative bg-gray-bgpicture w-330 h-401 md:h-330 lg:w-451 lg:h-547 lg:ml-135 mt-40 lg:mt-157 z-10">
-        <div class="bg-gray-bgrectangle2 invisible lg:visible w-102 h-40.4 float-right"></div> 
-        <div class="absolute invisible lg:visible bg-orange-bgrectangle w-61 h-61 -left-61 top-60"></div>
-        <div class="absolute invisible lg:visible bg-gray-bgrectangle6 w-102 h-63 top-40 left-322"></div>
-        
-        <div class="bg-gray-bgrectangle1 w-119 h-122"></div>
-        <div class="flex flex-col">
-          <div class="absolute flex flex-row left-322 top-103">
-            <div class="bg-orange-bgrectangle invisible lg:visible w-38.33 h-38.33"></div>
-            <div class="bg-gray-bgheader invisible lg:visible w-38.33 h-38.33"></div>
-          </div>
-          <div class="absolute invisible lg:visible bg-white w-38.33 h-38.33 left-322 top-141"></div>
-        </div>
-        
-        
-        
-        <img
-          src="../assets/hero/guy1.svg"
-          class="absolute w-272 h-374 md:h-272 lg:w-300 lg:h-520 top-27 left-55"
-        />
-        <div class="absolute invisible lg:visible bg-white w-61 h-61 top-122"></div>
-        
-        <div class="absolute invisible lg:visible bg-gray-bgrectangle4 w-27 h-169 left-425 top-103"></div>
-        <div class="absolute invisible lg:visible bg-gray-bgrectangle3 w-93 h-93 left-425 top-272"></div>
-        
-        
-        <div class="absolute invisible lg:visible bg-gray-bgrectangle3 w-143 h-142 top-330 -left-144"></div>
-        <div class="absolute invisible lg:visible bg-gray-bgrectangle3 w-54 h-54 left-425 top-547"></div>
-        <div class="absolute invisible lg:visible bg-white w-89 h-88 left-220 top-547"></div>
-        <div class="absolute invisible lg:visible bg-gray-bgrectangle5 w-36 h-38.33 top-509 -left-94"></div>
-        <div class="absolute invisible lg:visible bg-gray-bgrectangle6 w-29.93 h-29.93 top-517 left-330"></div>
-        <div class="absolute invisible lg:visible bg-orange-bgrectangle w-29.93 h-29.93 top-487 left-300"></div>
-        <div class="absolute invisible lg:visible bg-white w-29.93 h-29.93 top-457 left-330"></div>
-        <div class="absolute invisible lg:visible bg-gray-bghero w-29.93 h-29.93 top-487 left-360"></div>
+       <div>
+        <img src="../assets/hero/image.svg" alt="" class="mt-157 w-665 h-601 ml-36">
       </div>
     </div>
   </div>
